9a088657a571ac8870a03a7b828c240078e12283
shim0311 <sjt980311@gmail.com>
2026-01-21 17:40:21 +0900

    네비바 로고를 'Luka's Portfolio'로 변경

--- app/components/navbar.jsx
+++ app/components/navbar.jsx
@@ -10,7 +10,7 @@ function Navbar() {
           <Link
             href="/"
             className=" text-[#16f2b3] text-3xl font-bold">
-            심재탁
+            Luka's Portfolio
           </Link>
         </div>
 
